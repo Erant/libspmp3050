214a017068a312f9ea226ffcdfd3ff14de5fa821
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-08 21:57:12 +0000

    Silly bugfixes. Compiles now.

--- trunk/src/cbl/pwr.c
+++ trunk/src/cbl/pwr.c
@@ -2,8 +2,8 @@
 #include "../spmp3050/spmp3050.h"
 
 void PWR_UnitOn(){
-	(*(uint32_t*)PWR_REG) |= 0x02;
-	(*(uint32_t*)(IO_BASE + 0x1F0) |= 0x02;
+	*((uint32_t*)PWR_REG) |= 0x02;
+	(*(uint32_t*)(IO_BASE + 0x1F0)) |= 0x02;
 	(*(uint32_t*)(PWR_REG + 4)) &= ~0xFF;
 	(*(uint32_t*)(PWR_REG + 4)) |= 0x02;
 }
