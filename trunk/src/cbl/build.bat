f3e1093b12cd14ad2ea3fec9cdc905cf14e413bc
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-08 22:42:13 +0000

    So sleepy... It's ugly code, but it works. Kinda.

--- trunk/src/cbl/build.bat
+++ trunk/src/cbl/build.bat
@@ -1,6 +1,6 @@
 @echo off
 arm-eabi-gcc -Wall -c pwr.c cbl.c uart.c
-arm-eabi-ld -T ldscript pwr.o cbl.o uart.o -o cbl.elf
+arm-eabi-ld -T ldscript cbl.o pwr.o uart.o -o cbl.elf
 arm-eabi-objcopy -O binary cbl.elf cbl.bin
 rm *.elf
 rm *.o
--- trunk/src/cbl/cbl.c
+++ trunk/src/cbl/cbl.c
@@ -4,6 +4,7 @@
 #include "pwr.h"
 
 void printString(char* str);
+void printMemory(uint32_t* ptr);
 char* itoa(uint32_t val, char* buf);
 
 char itoa_buf[9];
@@ -12,10 +13,9 @@ char itoa_buf[9];
 void _start(){
 	uint8_t recv;
 	UART_Init(1);
-	PWR_UnitOn();
 	
 	while(1){
-		printString("Welcome to Crappy BootLoader (CBL) v0.01 (build 12)\r\n\n");
+		printString("Welcome to Crappy BootLoader (CBL) v0.01 (build 15)\r\n\n");
 		printString("1. Poke memory\r\n");
 		printString("2. Peek memory\r\n");
 		printString("3. Upload binary\r\n");
@@ -28,21 +28,32 @@ void _start(){
 		recv -= 0x30;
 		switch(recv){
 			case 1:	// POKE
+				*((uint8_t*)0x1000B320) = 0x00;
+				*((uint8_t*)0x1000B321) = 0x00;
+				*((uint8_t*)0x1000B322) = 0x00;
+				*((uint8_t*)0x1000B323) = 0x00;
+				PWR_UnitOn();
 				printString("Enter an 8 digit hexadecimal address to poke:\r\n");
 				// TODO
 				printString("Enter a hexadecimal number to poke:\r\n");
 				// TODO
 				break;
 			case 2: // PEEK
-				printString("0x1000B064: 0x");
-				uint32_t val = *((uint8_t*)0x1000B064) & 0xFF;
-				printString(itoa(val, itoa_buf));
-				printString("\r\n");
+				printMemory((uint32_t*)0x100001EC);
+				printMemory((uint32_t*)0x100001F0);
+				printMemory((uint32_t*)0x10001100);
+				printMemory((uint32_t*)0x10001108);
+				printMemory((uint32_t*)0x1000B064);
+				printMemory((uint32_t*)0x1000B068);
+				printMemory((uint32_t*)0x1000B320);
 				break;
 			case 3: // Upload binary
 				((void (*)(void))0x240CB734)();
 				break;
 			case 4: // Continue boot.
+				*((uint8_t*)GPIO_BASE) = 0x00;
+				*((uint8_t*)(GPIO_BASE + 0x08)) = 0x4E;
+				*((uint8_t*)(IO_BASE + 0x1EC)) &= ~0x4E;
 				break;
 			case 5: // Continue boot.
 				PWR_UnitOff();
@@ -65,4 +76,12 @@ char* itoa(uint32_t val, char* buf){
 	}
 	buf[8] = '\0';
 	return buf;
+}
+
+void printMemory(uint32_t* ptr){
+	printString("0x");
+	printString(itoa((uint32_t)ptr, itoa_buf));
+	printString(": 0x");
+	printString(itoa(*ptr, itoa_buf));
+	printString("\r\n");
 }
