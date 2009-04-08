d19b5373bf493d61f5330af20489022b8235a80c
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-08 22:42:13 +0000

    So sleepy... It's ugly code, but it works. Kinda.

--- src/cbl/pwr.c
+++ src/cbl/pwr.c
@@ -2,12 +2,12 @@
 #include "../spmp3050/spmp3050.h"
 
 void PWR_UnitOn(){
-	*((uint32_t*)PWR_REG) |= 0x02;
-	(*(uint32_t*)(IO_BASE + 0x1F0)) |= 0x02;
-	(*(uint32_t*)(PWR_REG + 4)) &= ~0xFF;
-	(*(uint32_t*)(PWR_REG + 4)) |= 0x02;
+	*((uint8_t*)PWR_REG) |= 0x02;
+	(*(uint8_t*)(PWR_REG + 4)) |= 0x02;
+	(*(uint8_t*)(IO_BASE + 0x1F0)) &= ~0x02;
+	
 }
 
 void PWR_UnitOff(){
-	(*(uint32_t*)PWR_REG) = 0x00;
+	(*(uint8_t*)PWR_REG) = 0x00;
 }
