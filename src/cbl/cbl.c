4cd0c3963ac03670a40bbef065092ba36b0b2faa
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-10 20:59:28 +0000

    Added peek and poke functionality, fixed the PWR functions, and added extra helper functions to cbl.c

--- src/cbl/cbl.c
+++ src/cbl/cbl.c
@@ -1,15 +1,20 @@
 #include <stdint.h>
+#include <stddef.h>
 #include "../spmp3050/spmp3050.h"
 #include "uart.h"
 #include "pwr.h"
 #include "xmodem.h"
 
 void printString(char* str);
+char* receiveString(char* buf);
 void printMemory(uint32_t* ptr);
+void printhex(uint8_t *buf, size_t n, uint32_t addr, int linew);
+uint32_t atoi(char* buf);
 char* itoa(uint32_t val, char* buf);
+char* ctoa(uint8_t val, char* buf);
 
 char itoa_buf[9];
-
+char rx_buf[256];
 
 // -- convenience wrappers to help with the xmodem implementation.
 
@@ -30,7 +35,7 @@ void serialPutByte( unsigned char c )
   UART_SendByte( 1, c );
 }
 
-unsigned char* xmodemWritePointer;
+uint8_t* xmodemWritePointer;
 
 int xmodemWriterHelper( unsigned char * data, int size )
 {
@@ -47,16 +52,18 @@ int xmodemWriterHelper( unsigned char * data, int size )
 
 void _start(){
 	uint8_t recv;
+	uint32_t prev_poke = 0, prev_peek = 0, offset = 0x24003000;
 	UART_Init(1);
+	PWR_UnitOn();
+	printString("\r\n");
 	
 	while(1){
-		printString("Welcome to Crappy BootLoader (CBL) v0.01 (build 15)\r\n\n");
+		printString("Welcome to Crappy BootLoader (CBL) v0.01 (build 16)\r\n\n");
 		printString("1. Poke memory\r\n");
 		printString("2. Peek memory\r\n");
 		printString("3. Upload binary\r\n");
 		printString("4. Continue boot\r\n");
 		printString("5. Turn unit off\r\n\n");
-		printString("6. XMODEM hackery\r\n\n");
 		do{
 			while(UART_ReceiveBufferEmpty(1));
 			recv = UART_ReceiveByte(1);
@@ -64,49 +71,148 @@ void _start(){
 		recv -= 0x30;
 		switch(recv){
 			case 1:	// POKE
-				*((uint8_t*)0x1000B320) = 0x00;
-				*((uint8_t*)0x1000B321) = 0x00;
-				*((uint8_t*)0x1000B322) = 0x00;
-				*((uint8_t*)0x1000B323) = 0x00;
-				PWR_UnitOn();
-				printString("Enter an 8 digit hexadecimal address to poke:\r\n");
-				// TODO
-				printString("Enter a hexadecimal number to poke:\r\n");
-				// TODO
+				printString("Enter an 8 digit hexadecimal address to poke (0x");
+				printString(itoa(prev_poke, itoa_buf));
+				printString(") :\r\n0x");				
+				if(receiveString(rx_buf) < 0)
+					break;
+					
+				if(*rx_buf)
+					prev_poke = atoi(rx_buf);
+				
+				printString("\r\nEnter a 2 digit hexadecimal number to poke:\r\n0x");
+				*((uint8_t*)prev_poke) = atoi(receiveString(rx_buf));
+				printString("\r\n");
 				break;
 			case 2: // PEEK
-				printMemory((uint32_t*)0x100001EC);
-				printMemory((uint32_t*)0x100001F0);
-				printMemory((uint32_t*)0x10001100);
-				printMemory((uint32_t*)0x10001108);
-				printMemory((uint32_t*)0x1000B064);
-				printMemory((uint32_t*)0x1000B068);
-				printMemory((uint32_t*)0x1000B320);
+				printString("Enter an 8 digit hexadecimal address to peek (0x");
+				printString(itoa(prev_peek, itoa_buf));
+				printString(") :\r\n0x");
+				if(receiveString(rx_buf) < 0)
+					break;
+				if(*rx_buf)
+					prev_peek = atoi(rx_buf);
+				
+				printString("\r\nEnter a hexadecimal length to read:\r\n0x");
+				int read_length = atoi(receiveString(rx_buf));
+				printString("\r\n");
+				printhex((uint8_t*)prev_peek, read_length, prev_peek, 16);
+				printString("Press any button to continue.\r\n");
+				while(UART_ReceiveBufferEmpty(1));
+				UART_ReceiveByte(1);
 				break;
 			case 3: // Upload binary
-				((void (*)(void))0x240CB734)();
+				printString("Enter an 8 digit hexadecimal address to load your binary to (0x");
+				printString(itoa(offset, itoa_buf));
+				printString(") :\r\n0x");
+				if(receiveString(rx_buf) < 0)
+					break;
+				if(*rx_buf)
+					offset = atoi(rx_buf);
+					
+				// xmodem transfer.
+				xmodemInit( serialPutByte, serialGetByte );
+				xmodemWritePointer = (uint8_t*)offset;
+				xmodemReceive( xmodemWriterHelper );
+				
 				break;
 			case 4: // Continue boot.
-				*((uint8_t*)GPIO_BASE) = 0x00;
-				*((uint8_t*)(GPIO_BASE + 0x08)) = 0x4E;
-				*((uint8_t*)(IO_BASE + 0x1EC)) &= ~0x4E;
+				printString("Enter an 8 digit hexadecimal address to jump to (0x");
+				printString(itoa(offset, itoa_buf));
+				printString(") :\r\n0x");
+				if(receiveString(rx_buf) < 0)
+					break;
+				if(*rx_buf)
+					offset = atoi(rx_buf);
+					
+				((void (*)(void))offset)();
 				break;
-			case 5: // Continue boot.
+			case 5: // Abort boot.
 				PWR_UnitOff();
+				while(1);
 				break;
-            case 6:
-              // xmodem transfer.
-              xmodemInit( serialPutByte, serialGetByte );
-              xmodemWritePointer = 0x100;
-              xmodemReceive( xmodemWriterHelper );
-              break;
 		}
 	}
 }
 
+// All helper functions, because we don't have a libc yet.
+void printhex(uint8_t *buf, size_t n, uint32_t addr, int linew){
+	size_t pos,i, l, j;
+	for (pos=0;pos<n;pos+=linew,addr+=linew){
+		l = pos+linew>n?n:pos+linew;
+		printString(itoa(addr, itoa_buf));
+		printString("   ");
+		for (i=pos,j=0;i<pos+linew;i++,j++){
+			if (i<n){
+				printString(ctoa(buf[i],itoa_buf));
+				printString(" ");
+			}
+			else
+			{
+				printString("   ");
+			}
+			if(!((j+1)%4))
+				printString(" ");
+		}
+		if (linew%4)
+			printString(" ");
+		for (i=pos;i<l;i++){
+			if(buf[i]>=0x20&&buf[i]<=0x7F)
+				UART_SendByte(1, buf[i]);
+			else
+				UART_SendByte(1, '.');
+		}
+		printString("\r\n");
+	}
+}
+
+
 void printString(char* str){
 	for(; *str != '\0'; str++)
 		UART_SendByte(1, *str);
+}
+
+char* receiveString(char* buf){
+	char* ret = buf;
+	do{
+		while(UART_ReceiveBufferEmpty(1));
+		*buf = UART_ReceiveByte(1);
+		if(*buf>=0x20&&*buf<=0x7F){
+			UART_SendByte(1, *buf);
+		}
+		
+		if(*buf == 0x03){	// CTRL + C
+			*buf = 0;
+			return (char*)-1;
+		}
+		
+		if(*buf == '\b' && buf > ret){
+			printString("\b \b");
+		}
+		else
+		{
+			buf++;
+		}
+	}while(*(buf - 1) != '\r');
+	*(buf - 1) = '\0';
+	return ret;
+}
+
+uint32_t atoi(char* buf){
+	uint32_t ret = 0;
+	while((*buf >= '0' && *buf <= '9') || (*buf >= 'a' && *buf <= 'f') || (*buf >= 'A' && *buf <= 'F')){
+		if(*buf >= 'a' && *buf <= 'f')
+			*buf -= 'a' - 'A';
+		if(*buf >= 'A' && *buf <= 'F')
+			*buf -= 0x7;
+		*buf -= 0x30;
+		
+		ret = ret << 4;
+		ret |= *buf;
+		
+		buf++;
+	}
+	return ret;
 }
 
 char lut[] = "0123456789ABCDEF";
@@ -117,6 +223,13 @@ char* itoa(uint32_t val, char* buf){
 		val = val >> 4;
 	}
 	buf[8] = '\0';
+	return buf;
+}
+
+char* ctoa(uint8_t val, char* buf){
+	buf[2] = '\0';
+	buf[1] = lut[val & 0xF];
+	buf[0] = lut[(val >> 4) & 0xF];
 	return buf;
 }
 
--- src/cbl/pwr.c
+++ src/cbl/pwr.c
@@ -2,6 +2,7 @@
 #include "../spmp3050/spmp3050.h"
 
 void PWR_UnitOn(){
+	*((uint32_t*)(PWR_BASE + 0x320)) = 0x00;
 	*((uint8_t*)PWR_REG) |= 0x02;
 	(*(uint8_t*)(PWR_REG + 4)) |= 0x02;
 	(*(uint8_t*)(IO_BASE + 0x1F0)) &= ~0x02;
