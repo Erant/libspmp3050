23227dd23c32e6f029b4edacbacc498f06a21fa9
zouookmoetenwerken <zouookmoetenwerken@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-10 08:41:11 +0000

    added xmodem hackery (haven't tested compilation since my arm compiler still complains)

--- src/cbl/build.bat
+++ src/cbl/build.bat
@@ -1,6 +1,6 @@
 @echo off
-arm-eabi-gcc -Wall -c pwr.c cbl.c uart.c
-arm-eabi-ld -T ldscript cbl.o pwr.o uart.o -o cbl.elf
+arm-eabi-gcc -Wall -c pwr.c cbl.c uart.c xmodem.c
+arm-eabi-ld -T ldscript cbl.o pwr.o uart.o xmodem.o -o cbl.elf
 arm-eabi-objcopy -O binary cbl.elf cbl.bin
 rm *.elf
 rm *.o
---
+++ src/cbl/xmodem.h
@@ -0,0 +1,15 @@
+#ifndef XMODEM_H
+#define XMODEM_H
+
+void xmodemInit(void (*sendbyte_func)(unsigned char c), int (*getbyte_func)(void));
+long xmodemReceive( int (*write)(unsigned char* buffer, int size) );
+int xmodemInTime(unsigned short timeout);
+void xmodemInFlush(void);
+int xmodemCrcCheck(int crcflag, const unsigned char *buffer, int size);
+
+#define XMODEM_ERROR_OUTOFSYNC -1
+#define XMODEM_ERROR_REMOTECANCEL -2
+#define XMODEM_ERROR_RETRYEXCEED -3
+#define XMODEM_TIMEOUT_DELAY 1000
+
+#endif
