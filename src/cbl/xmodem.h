7167d4ab2c8960ecd0dd9c3d9685415840e49358
zouookmoetenwerken <zouookmoetenwerken@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-10 15:03:07 +0000

    removed duplicate definitions

--- src/cbl/xmodem.h
+++ src/cbl/xmodem.h
@@ -3,9 +3,6 @@
 
 void xmodemInit(void (*sendbyte_func)(unsigned char c), int (*getbyte_func)(void));
 long xmodemReceive( int (*write)(unsigned char* buffer, int size) );
-int xmodemInTime(unsigned short timeout);
-void xmodemInFlush(void);
-int xmodemCrcCheck(int crcflag, const unsigned char *buffer, int size);
 
 #define XMODEM_ERROR_OUTOFSYNC -1
 #define XMODEM_ERROR_REMOTECANCEL -2
