6548b0ba016bd5b87266c0bf7f2a4f8c31ee5cc8
zouookmoetenwerken <zouookmoetenwerken@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-12 14:23:45 +0000

    added makefile

--- trunk/src/cbl/gpio.c
+++ trunk/src/cbl/gpio.c
@@ -1,15 +1,15 @@
 #include <stdint.h>
 #include "../spmp3050/spmp3050.h"
 
-extern inline void GPIO_Init(){
+inline void GPIO_Init(){
 	GPIO_DISABLE = 0x00;
 }
 
-extern inline void GPIO_UnitOn(){
+inline void GPIO_UnitOn(){
 	GPIO_A_DIR |= 0x02;
 	GPIO_A_OUT |= 0x02;
 }
 
-extern inline void GPIO_UnitOff(){
+inline void GPIO_UnitOff(){
 	GPIO_A_OUT &= ~0x02;
 }
