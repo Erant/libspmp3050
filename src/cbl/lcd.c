f60908357985a2127733feece99702bdd8952c55
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-04-13 14:51:56 +0000

    New and improved LCD code! Now with actual LCD functionality. Much thanks goes to AleMaxx.

--- src/cbl/lcd.c
+++ src/cbl/lcd.c
@@ -7,3 +7,50 @@ void LCD_SetBacklight(int val){
 	else
 		DEV_ENABLE_OUT &= ~0x8;
 }
+
+void LCD_CtrlWrite(int reg, int val){
+	LCD_AddrWrite(reg);
+	LCD_CmdWrite(val);
+}
+
+
+
+void LCD_AddrWrite(uint16_t val){
+	volatile uint8_t* lcd_base = LCD_BASE;
+	
+	LCD_DATA = val;
+	
+	lcd_base[0xE4] = ((val & 0x30000) >> 14) | (lcd_base[0xE4] & 0x3);
+	
+	LCD_DATA_DIR |= LCD_DATA_OUT;
+	
+	LCD_CTRL = (LCD_CTRL & ~0x3C) | LCD_CS;
+	
+	LCD_CTRL |= LCD_WR;
+	delay(3);
+	LCD_CTRL &= ~LCD_WR;
+	
+	LCD_DATA_DIR |= LCD_DATA_OUT;
+	
+	LCD_CTRL |= LCD_nRS;
+}
+
+void LCD_CmdWrite(uint16_t val){
+	volatile uint8_t* lcd_base = LCD_BASE;
+	
+	LCD_DATA = val;
+	
+	lcd_base[0xE4] = ((val & 0x30000) >> 14) | (lcd_base[0xE4] & 0x3);
+	
+	LCD_DATA_DIR |= LCD_DATA_OUT;
+	
+	LCD_CTRL = (LCD_CTRL & ~0x3C) | LCD_CS | LCD_nRS;
+	
+	LCD_CTRL |= LCD_WR;
+	delay(3);
+	LCD_CTRL &= ~LCD_WR;
+	
+	lLCD_DATA_DIR |= LCD_DATA_OUT;
+	
+	LCD_CTRL &= ~LCD_nRS;
+}
