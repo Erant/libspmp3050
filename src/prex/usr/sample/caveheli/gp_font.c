9c8eded9fca9477cf306162c14292bc50caedaef
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-05-14 20:24:46 +0000

    Added 'caveheli', a GP32 game that should be easy to port. Currently hitting the RAMdisk size limit though...

---
+++ src/prex/usr/sample/caveheli/gp_font.c
@@ -0,0 +1,44 @@
+#include "font.c"
+#include <string.h>
+
+void drawchar(u16 *sprite, int x, int y, u16 color, int chr, u16 *framebuffer);
+void text(int x, int y, u16 color, char *buffer, u16 *framebuffer);
+
+void gp_drawPixel16(int x, int y, u16 color, void* fb){
+	u16* framebuf = (u16*) fb;
+	framebuf[240 * x + y] = color;
+}
+
+void drawchar(u16 *sprite, int x, int y, u16 color, int chr, u16 *framebuffer)
+{
+        int xx, yy, xpos, ypos;
+        u16 pixel;
+
+        xpos=(chr % 32)*8;
+        ypos=(chr / 32)*16;
+        
+        for (yy=0;yy<16;yy++)
+        {
+                for (xx=0;xx<8;xx++)
+                {
+                        pixel=sprite[xpos+ypos*256];
+			if (xx+x<320 && xx+x>0)
+                        if(pixel==0) gp_drawPixel16(xx+x,yy+y,color,framebuffer);
+                        xpos++;
+                }
+                xpos=xpos-8;
+                ypos++;
+        }
+}
+
+void text(int x, int y, u16 color, char *buffer, u16 *framebuffer)
+{
+        int i, len, c;
+        len=strlen(buffer);
+        for(i=0;i<len;i++)
+        {
+                c=buffer[i];
+                drawchar((u16 *)font,x+i*8,y,color,c, framebuffer);
+        }
+}
+
