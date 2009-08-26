fc169d73d06a489293b7e08186177ef96ac086ea
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-08-26 05:28:51 +0000

    Deleted the old mmu port.

--- trunk/src/prex-mmu/sys/arch/arm/include/platform.h
+++
@@ -1,9 +0,0 @@
-#if defined(__gba__)
-#include "../gba/platform.h"
-#elif defined(__integrator__)
-#include "../integrator/platform.h"
-#elif defined(__spmp__)
-#include "../spmp/platform.h"
-#else
-#error platform not supported
-#endif
