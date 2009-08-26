0f4c2e43e42347d541eb9c3158d9c1cd854047f4
erantimus <erantimus@43f8b19c-239f-11de-8bc4-bb72633a7edc>
2009-08-26 05:28:51 +0000

    Deleted the old mmu port.

--- src/prex-mmu/boot/arm/spmp/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UART_H
-#define _UART_H
-
-void UART_Init(int uart);
-void UART_SendByte(int uart, unsigned char byte);
-uint8_t UART_ReceiveByte(int uart);
-int	UART_ReceiveBufferEmpty(int uart);
-
-#endif
